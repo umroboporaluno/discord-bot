98d86e91d53237d44a5c6a69ed0c8d9b8b5e4bd8
Luis Martins <chateadorn@gmail.com>
2023-03-10 12:53:42 -0300

    removendo try/catch + imports desnecessários

--- src/main/java/br/natalnet/ura/bot/BotApplication.java
+++ src/main/java/br/natalnet/ura/bot/BotApplication.java
@@ -1,20 +1,7 @@
 package br.natalnet.ura.bot;
 
 import br.natalnet.ura.bot.database.Redis;
-import br.natalnet.ura.bot.event.ButtonClickEvent;
-import br.natalnet.ura.bot.controller.CommandController;
-import br.natalnet.ura.bot.event.MenuClickEvent;
-import br.natalnet.ura.bot.listener.DoubtsEmbedMessageListener;
-import br.natalnet.ura.bot.listener.RulesEmbedMessageListener;
 import lombok.Getter;
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.OnlineStatus;
-import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
-import net.dv8tion.jda.api.sharding.ShardManager;
-import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 import javax.security.auth.login.LoginException;
 
@@ -26,7 +13,7 @@ public class BotApplication {
     @Getter
     private static Redis redis;
 
-    public static void main(String[] args) throws LoginException {
+    public static void main(String[] args) {
         system = new BotSystem();
         redis = new Redis();
     }
