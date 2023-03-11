fd94b9841f97f7264e3f77fc685403c46be916a4
Luis Martins <chateadorn@gmail.com>
2023-03-10 23:00:31 -0300

    start of redispubsub

---
+++ src/main/java/br/natalnet/ura/bot/controller/RedisPubSubController.java
@@ -0,0 +1,20 @@
+package br.natalnet.ura.bot.controller;
+
+import br.natalnet.ura.bot.entity.Member;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import redis.clients.jedis.JedisPubSub;
+
+public class RedisPubSubController extends JedisPubSub {
+
+    @Override
+    public void onMessage(String channel, String message) {
+
+        Gson gson = new GsonBuilder().create();
+
+        if (channel.equals("cadastro")) {
+
+            Member member = gson.fromJson(message, Member.class);
+        }
+    }
+}
