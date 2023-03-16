12dc0a1e56ee05bc8fa799caf5d04857dc1d6d53
Luis Martins <chateadorn@gmail.com>
2023-03-16 12:32:57 -0300

    adicionando novo sistema do paho mqtt

--- src/main/java/br/natalnet/ura/bot/database/MQTT.java
+++ src/main/java/br/natalnet/ura/bot/database/MQTT.java
@@ -1,36 +1,142 @@
 package br.natalnet.ura.bot.database;
 
-import br.natalnet.ura.bot.controller.MQTTPubSubController;
-import com.hivemq.client.mqtt.datatypes.MqttQos;
-import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
 import lombok.Getter;
+import org.eclipse.paho.mqttv5.client.*;
+import org.eclipse.paho.mqttv5.client.persist.MqttDefaultFilePersistence;
+import org.eclipse.paho.mqttv5.common.MqttException;
+import org.eclipse.paho.mqttv5.common.MqttMessage;
+import org.eclipse.paho.mqttv5.common.packet.MqttProperties;
 
 import java.util.UUID;
 
 @Getter
-public class MQTT {
+public class MQTT implements MqttCallback {
 
-    private final Mqtt5Client client;
+    private final String serverUri;
 
-    private final MQTTPubSubController controller;
+    private MqttClient client;
 
-    public MQTT() throws InterruptedException {
+    private final MqttConnectionOptions options;
 
-        client = Mqtt5Client.builder()
-                .identifier(UUID.randomUUID().toString())
-                .serverHost("127.0.0.1")
-                .build().toBlocking();
+    public MQTT(String serverUri) {
+        this.serverUri = serverUri;
 
-        client.toBlocking().connectWith().keepAlive(5).send();
+        this.options = new MqttConnectionOptions();
 
-        controller = new MQTTPubSubController();
+        options.setConnectionTimeout(3);
+        options.setKeepAliveInterval(5);
+        options.setAutomaticReconnect(true);
+        options.setCleanStart(false);
     }
 
-    public void publish(String topic, String message) {
-        client.toBlocking().publishWith().topic(topic).qos(MqttQos.AT_LEAST_ONCE).payload(message.getBytes()).send();
+    public void connect() {
+        try {
+            System.out.println("Connecting to " + getServerUri() + " MQTT broker...");
+
+            client = new MqttClient(serverUri, UUID.randomUUID().toString(), new MqttDefaultFilePersistence(System.getProperty("java.io.tmpdir")));
+
+            client.setCallback(this);
+
+            client.connect(options);
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void subscribe(String topic) {
-        client.toBlocking().subscribeWith().topicFilter(topic).qos(MqttQos.AT_LEAST_ONCE).send();
+    public void disconnect() {
+
+        if (client == null || !client.isConnected())
+            return;
+
+        try {
+            client.disconnect();
+            client.close();
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public IMqttToken subscribe(int qos, IMqttMessageListener listener, String... topics) {
+
+        if (client == null || topics.length == 0)
+            return null;
+
+        int size = topics.length;
+
+        int[] i = new int[size];
+
+        IMqttMessageListener[] listeners = new IMqttMessageListener[size];
+
+        for (int x = 0; x < size; x++) {
+            i[x] = qos;
+
+            listeners[x] = listener;
+        }
+
+        try {
+            return client.subscribe(topics, i, listeners);
+        } catch (MqttException e) {
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
+    public void unsubscribe(String... topics) {
+
+        if (client == null || !client.isConnected() || topics.length == 0)
+            return;
+
+        try {
+            client.unsubscribe(topics);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void publish(String topic, byte[] payload, int qos, boolean retained) {
+
+        try {
+
+            if (!client.isConnected())
+                return;
+
+            client.publish(topic, payload, qos, retained);
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void mqttErrorOccurred(MqttException exception) {
+
+    }
+
+    @Override
+    public void messageArrived(String topic, MqttMessage message) throws Exception {
+        System.out.println(message);
+    }
+
+    @Override
+    public void deliveryComplete(IMqttToken token) {
+
+    }
+
+    @Override
+    public void connectComplete(boolean reconnect, String serverURI) {
+        System.out.println("MQTT " + (reconnect ? "reconnected" : "connected") + " with success in " + serverURI);
+    }
+
+    @Override
+    public void disconnected(MqttDisconnectResponse disconnectResponse) {
+        System.out.println("Connection lost, trying reconnect...");
+    }
+
+    @Override
+    public void authPacketArrived(int reasonCode, MqttProperties properties) {
+
     }
 }
