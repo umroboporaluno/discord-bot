64870c55b5153ebb5bbf0f5221ac89170b295ff9
Luis Martins <chateadorn@gmail.com>
2024-04-11 21:18:03 -0300

    feat: adicionando padrao das mensagens de boas vindas

--- src/main/java/br/natalnet/ura/bot/message/WelcomeMessagePattern.java
+++ src/main/java/br/natalnet/ura/bot/message/WelcomeMessagePattern.java
@@ -1,2 +1,18 @@
-package br.natalnet.ura.bot.message;public enum WelcomeMessagePattern {
+package br.natalnet.ura.bot.message;
+
+import lombok.Getter;
+
+@Getter
+public enum WelcomeMessagePattern {
+
+    GRADUANDOS(":robot: - **Graduandos**", ":alarm_clock: Horário: **18:35 até às 20:30** \n:calendar: Início das aulas: **12/04/2024** \n:round_pushpin: Local: **Laboratório 1 ECT, 2 piso**"),
+    PROFESSORES(":robot: - **Ensino Médio**", ":alarm_clock: Horário: **8:30 até às 11:30** \n:calendar: Início das aulas: **13/04/2024** \n:round_pushpin: Local: **Laboratório 1 ECT, 2 piso**"),
+    ENSINO_MEDIO("", "");
+
+    private final String title, subtitle;
+
+    WelcomeMessagePattern(String title, String subtitle) {
+        this.title = title;
+        this.subtitle = subtitle;
+    }
 }
